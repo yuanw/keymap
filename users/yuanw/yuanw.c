df050bf3caad33f0b2cf4ed1a1d0f19331016462
yuanw <me@yuanwang.ca>
2022-10-02 11:32:39 -0600

    add heatmap

--- users/yuanw/yuanw.c
+++ users/yuanw/yuanw.c
@@ -76,17 +76,17 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    #ifdef CONSOLE_ENABLE
-        uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n",
-             keycode,
-             record->event.key.row,
-             record->event.key.col,
-             get_highest_layer(layer_state),
-             record->event.pressed,
-             get_mods(),
-             get_oneshot_mods(),
-             record->tap.count
-             );
-    #endif
+ #ifdef CONSOLE_ENABLE
+uprintf("0x%04X,%u,%u,%u,%d,0x%02X,0x%02X,%u\n",
+         keycode,
+         record->event.key.row,
+         record->event.key.col,
+         layer_state|default_layer_state,
+         record->event.pressed,
+         get_mods(),
+         get_oneshot_mods(),
+         record->tap.count
+         );
+#endif
     return true;
 }
